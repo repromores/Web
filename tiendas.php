0d63c49b575da995746834b7b209d87198d7c915
ramon kamibayashi <ramon.kamibayashi@mores.es>
2012-08-06 19:10:39 +0200

    tiendas

---
+++ tiendas.php
@@ -0,0 +1,46 @@
+﻿<?php include "inc/config.php"; ?>
+<?php include "inc/head.php"; ?>
+<title>morés - Tiendas</title>
+<?php
+  // $h1text : variable para fijar el H1 en cada pagina para hacerlo único y aprovechar mejor el SEO
+  $h1text = "Tiendas - morés";
+ ?>
+<?php include "inc/menu.php"; ?>
+
+  <div class="span10">
+  	<div class="content">
+  		<h2>Edificio Covadonga</h2>
+<p>
+Con el fin de continuar avanzando en el proyecto de Morés como empresa puntera en el sector de la impresión digital y de crear las bases del Morés del futuro, se abre a principios de 2008 un centro de producción a gran escala en la parcela nº 2 del Parque Tecnológico de Asturias.
+</p>
+<p>
+Desde un punto de vista operativo, se han trasladado a este edificio las secciones de producción más industriales de Morés: <strong>cartelería gran formato</strong>, <strong>rotulación</strong>, <strong>impresión</strong> e <strong>imprenta digital</strong>, y <strong>soportes expositivos</strong>.
+</p>
+<p>
+En el Edificio Covadonga del Parque Tecnológico de Asturias también se ubican los <strong>departamentos</strong> <strong>técnico</strong>, <strong>comercial</strong>, de <strong>administración</strong>, <strong>compras</strong> y <strong>logística</strong>.
+</p>
+<table class="minitable">
+<tbody><tr><td valign="top"><span class="gris">Dirección:</span></td><td>Parque Tecnológico de Asturias, Parcela 2 <br>33428 Cayés - Llanera (Asturias)</td></tr>
+<tr><td><span class="gris">Teléfono:</span></td><td>985 963 288 / 985 733 550</td></tr>
+<tr><td><span class="gris">Dpto. Comercial: </span></td><td>985 963 103 / 902 466 737</td></tr>
+<tr><td><span class="gris">Fax:</span></td><td>985 963 256</td></tr>
+<tr><td><span class="gris">Horario:</span></td><td>Lunes a Viernes de 9:00 a 13:30 y de 15:30 a 19:00.</td></tr>
+</tbody></table>
+
+<br>
+
+
+  	</div>
+  </div>
+
+  <div class="span3">
+    
+  <div class="envio-archivos">
+    <h2>Envío de Archivos</h2>
+    <p>Envía tus archivos y recogelos en cualquiera de nuestras tiendas</p>
+    <input type="submit" class="btn" value="Entrar">
+  </div>
+
+  </div>
+</div>
+<?php include "inc/footer.php"; ?>
